e42ea2a0b3a974c6ef3275e3178253a5f472fda5
LolaMelibaeva <lolam79@gmail.com>
2019-06-28 14:37:08 -0400

    my second commit

--- src/com/class01/Practice.java
+++ src/com/class01/Practice.java
@@ -4,6 +4,7 @@ public class Practice {
 	public static void main(String[] args) {
 		
 		System.out.println("This is my first code of demo line");
+		System.out.println("this is my second code of demo line--Lola");
 	}
 
 }
